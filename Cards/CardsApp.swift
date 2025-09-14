193df5d51a9d35a9ece3ed4c7a6e3bfccef1722d
Andrew Walker <andrewwalker1239@gmail.com>
2025-09-14 05:09:37 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Andrew Walker on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
