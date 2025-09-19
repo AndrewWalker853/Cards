2248e7678d8133a54154e48fb71c0bc032e51701
Andrew Walker <andrewwalker1239@gmail.com>
2025-09-19 12:22:39 -0400

    Chapter 19 Finished

--- Cards/Model/Card.swift
+++ Cards/Model/Card.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct Card: Identifiable {
-  let id = UUID()
+  var id = UUID()
   var backgroundColor: Color = .yellow
   var elements: [CardElement] = []
 
   mutating func addElement(uiImage: UIImage) {
-    let element = ImageElement(uiImage: uiImage)
+    let imageFilename = uiImage.save()
+    let element = ImageElement(
+      uiImage: uiImage,
+      imageFilename: imageFilename)
     elements.append(element)
+    save()
   }
 
   mutating func addElement(text: TextElement) {
@@ -32,9 +36,13 @@ struct Card: Identifiable {
   }
 
   mutating func remove(_ element: CardElement) {
+    if let element = element as? ImageElement {
+      UIImage.remove(name: element.imageFilename)
+    }
     if let index = element.index(in: elements) {
       elements.remove(at: index)
     }
+    save()
   }
 
   mutating func update(_ element: CardElement?, frameIndex: Int) {
@@ -44,5 +52,58 @@ struct Card: Identifiable {
         newElement.frameIndex = frameIndex
         elements[index] = newElement
     }
+  }
+
+  func save() {
+    do {
+      let encoder = JSONEncoder()
+      encoder.outputFormatting = .prettyPrinted
+      let data = try encoder.encode(self)
+      let filename = "\(id).rwcard"
+      let url = URL.documentsDirectory
+        .appendingPathComponent(filename)
+      try data.write(to: url)
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+}
+
+extension Card: Codable {
+  enum CodingKeys: CodingKey {
+    case id, backgroundColor, imageElements, textElements
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder
+      .container(keyedBy: CodingKeys.self)
+    let id = try container.decode(String.self, forKey: .id)
+    self.id = UUID(uuidString: id) ?? UUID()
+    elements += try container.decode(
+      [ImageElement].self, forKey: .imageElements)
+
+    // Challenge 2 - load the text elements
+    elements += try container.decode([TextElement].self, forKey: .textElements)
+
+    // Challenge 1 - load the background color
+    let components = try container.decode([CGFloat].self, forKey: .backgroundColor)
+    backgroundColor = Color.color(components: components)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(id.uuidString, forKey: .id)
+    let imageElements: [ImageElement] =
+      elements.compactMap { $0 as? ImageElement }
+    try container.encode(imageElements, forKey: .imageElements)
+
+    // Challenge 2 - save the text elements
+    let textElements: [TextElement] =
+      elements.compactMap { $0 as? TextElement }
+    try container.encode(textElements, forKey: .textElements)
+
+    // Challenge 1 - save the background color
+    let components = backgroundColor.colorComponents()
+    try container.encode(components, forKey: .backgroundColor)
   }
 }
